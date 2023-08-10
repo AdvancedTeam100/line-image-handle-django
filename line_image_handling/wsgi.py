b12c25d0972218a0fad9eb93f248403449e8bb13
AdvancedTeam100 <konadev0402@gmail.com>
2023-08-11 02:19:24 +0900

    change wsgi file

--- line_image_handling/wsgi.py
+++ line_image_handling/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'line_image_handling.settings')
 
 application = get_wsgi_application()
+
+app = application
